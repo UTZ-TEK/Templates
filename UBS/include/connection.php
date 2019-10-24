d2cf93c75b82675d0debe1c8c2f8603293b82e6e
Lex Jauregui <lextefasu@gmail.com>
2019-10-24 13:26:59 -0600

    cambio en connection.php

--- UBS/include/connection.php
+++ UBS/include/connection.php
@@ -1,15 +1,16 @@
 <?php
-/*
+
 	$servername = "utztek.com";
 	$username = "utztekco_dev";
 	$password = "UtzDev2019@";
 	$db 	  = "utztekco_base_systems";
-*/
+
+/*	
 	$servername = "25.61.55.238";
 	$username = "remote_root";
 	$password = "12345";
 	$db 	  = "new_system";
-	
+*/
 	// Create connection
 	$conn = mysqli_connect($servername, $username, $password, $db);
 
