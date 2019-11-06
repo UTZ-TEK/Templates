39a84bd03ee2db1051fa2fd3b37e34d241a0ae5f
Lex Jauregui <lextefasu@gmail.com>
2019-11-06 10:51:17 -0600

    ignore connection

--- utz_base/include/connection.php
+++ utz_base/include/connection.php
@@ -1,22 +1,22 @@
 <?php
-
+/*
 	$servername = "utztek.com";
 	$username = "utztekco_dev";
 	$password = "UtzDev2019@";
 	$db 	  = "utztekco_base_systems";
-
+*/
 /*
 	$servername = "localhost";
 	$username = "root";
 	$password = "";
 	$db 	  = "new_system";
 */
-/*
+
 	$servername = "25.61.55.238";
 	$username = "remote_root";
 	$password = "12345";
 	$db 	  = "new_system";
-*/
+
 
 	// Create connection
 	$conn = mysqli_connect($servername, $username, $password, $db);
