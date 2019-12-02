01f374b78b3e9e739d95850c72a7637ab9477e3b
Lex Jauregui <lextefasu@gmail.com>
2019-12-02 15:38:37 -0600

    git_ignore connection ubs-req

--- ubs-req/include/connection.php
+++ ubs-req/include/connection.php
@@ -1,9 +1,11 @@
 <?php
 
+/*
 	$servername = "utztek.com";
 	$username = "utztekco_dev";
 	$password = "UtzDev2019@";
 	$db 	  = "utztekco_ubs_req";
+*/	
 
 /*
 	$servername = "localhost";
@@ -11,12 +13,12 @@
 	$password = "";
 	$db 	  = "new_system";
 */
-/*
+
 	$servername = "25.61.55.238";
 	$username = "remote_root";
 	$password = "12345";
 	$db 	  = "ubs_req";
-*/
+
 
 	// Create connection
 	$conn = mysqli_connect($servername, $username, $password, $db);
