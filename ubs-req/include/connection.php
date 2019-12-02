b74cf2c8fb5d20838746229be085c61fe20cdef5
Lex Jauregui <lextefasu@gmail.com>
2019-12-02 15:21:39 -0600

    db changes

--- ubs-req/include/connection.php
+++ ubs-req/include/connection.php
@@ -1,22 +1,22 @@
 <?php
-/*
+
 	$servername = "utztek.com";
 	$username = "utztekco_dev";
 	$password = "UtzDev2019@";
-	$db 	  = "utztekco_base_systems";
-*/
+	$db 	  = "utztekco_ubs_req";
+
 /*
 	$servername = "localhost";
 	$username = "root";
 	$password = "";
 	$db 	  = "new_system";
 */
-
+/*
 	$servername = "25.61.55.238";
 	$username = "remote_root";
 	$password = "12345";
 	$db 	  = "ubs_req";
-
+*/
 
 	// Create connection
 	$conn = mysqli_connect($servername, $username, $password, $db);
